b5d4c7b135fb2aa0465a9bf6fd6a9324a03b1469
Will Firmiano <will.firmiano@gmail.com>
2016-09-06 15:29:06 -0300

    dataServer

--- js/app.js
+++ js/app.js
@@ -1,53 +1,20 @@
 /**
  * Created by william on 01/09/16.
  */
-var hello = new Vue({
-    el:'#hello',
+var app = new Vue({
+    el:'#app',
     data:{
-        msg:"Hello Vue!",
-        peoples:[
-            {name:"Maria"},
-            {name:"João"},
-            {name:"Fernandinho"},
-            {name:"Rábio"},
-            {name:"Jossé"}
-        ],
-        newElement:'',
-        elements:[],
-        objectA: {
-            'font-size':'30px'
-        },
-        objectB: {
-            color:"red"
-        },
-        myListForm: [],
-        myForm: {
-            name:'',
-            email:''
-        }
+        books:[]
     },
     methods:{
-        addElement: function(){
-            var title = this.newElement.trim();
-            if(title) {
-                this.elements.push({title:title});
-                this.newElement = "";
-            }
-        },
-        removeElement: function(e, index) {
-            e.preventDefault();
-            this.elements.splice(index,1);
-        },
-        myClick: function() {
-            alert('Click!');
-        },
-        myKeyUp: function() {
-            alert('MyKeyUp');
-        },
-        addForm: function() {
-            this.myListForm.push({name:this.myForm.name , email:this.myForm.email});
-            this.myForm.name = '';
-            this.myForm.email = '';
-        }
+
+    },
+    ready:function() {
+        var self = this;
+        self.$http.get('dataServer.json').then(function(response) {
+            //console.log(JSON.parse(response.data));
+            self.books = JSON.parse(response.data);
+        });
     }
+
 });
