229e4ddf0cc665ec0f95b2d7fc7d59fc31771f40
Will Firmiano <will.firmiano@gmail.com>
2016-09-06 17:22:58 -0300

    ordering

--- js/app.js
+++ js/app.js
@@ -4,10 +4,17 @@
 var app = new Vue({
     el:'#app',
     data:{
-        books:[]
+        books:[],
+        MySearch:'',
+        orderCol: 'id',
+        orderInverse:1
     },
     methods:{
-
+        filterOrderBy:function(e,col) {
+            this.orderCol = col;
+            this.orderInverse = this.orderInverse * -1;
+            console.log(this.orderInverse);
+        }
     },
     ready:function() {
         var self = this;
