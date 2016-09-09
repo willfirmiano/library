174adccfeaea4b2db1f9ba7da9a5f4faf88ff07d
Will Firmiano <will.firmiano@gmail.com>
2016-09-09 09:15:16 -0300

    finish

--- js/app.js
+++ js/app.js
@@ -7,20 +7,90 @@ var app = new Vue({
         books:[],
         MySearch:'',
         orderCol: 'id',
-        orderInverse:1
+        orderInverse:1,
+        pagination: {
+            maxPage:4,
+            current:1,
+            totalItens:0,
+            totalPages:5,
+            listNumbers:[],
+            listPagination:[]
+        }
     },
     methods:{
         filterOrderBy:function(e,col) {
+            e.preventDefault();
             this.orderCol = col;
             this.orderInverse = this.orderInverse * -1;
             console.log(this.orderInverse);
+        },
+        previous:function(e) {
+            e.preventDefault();
+
+            console.log('maxPage: ' + this.pagination.maxPage);
+            console.log('current: ' + this.pagination.current);
+            console.log('totalItens: ' + this.pagination.totalItens);
+            console.log('totalPages: ' + this.pagination.totalPages);
+            console.log('listNumbers: ' + this.pagination.listNumbers);
+            console.log('listPagination: ' + this.pagination.listPagination);
+
+            if(this.pagination.current === 1) {
+                return false;
+            }
+            this.pagination.current = this.pagination.current -1;
+            this.books = this.pagination.listPagination[this.pagination.current -1];
+        },
+        next:function(e) {
+            e.preventDefault();
+
+            console.log('maxPage: ' + this.pagination.maxPage);
+            console.log('current: ' + this.pagination.current);
+            console.log('totalItens: ' + this.pagination.totalItens);
+            console.log('totalPages: ' + this.pagination.totalPages);
+            console.log('listNumbers: ' + this.pagination.listNumbers);
+            console.log('listPagination: ' + this.pagination.listPagination);
+
+            if(this.pagination.current === this.pagination.totalPages) {
+                return false;
+            }
+            this.pagination.current = this.pagination.current +1;
+            this.books = this.pagination.listPagination[this.pagination.current -1];
+        },
+        pagePagination:function(e, current) {
+            e.preventDefault();
+
+            console.log('maxPage: ' + this.pagination.maxPage);
+            console.log('current: ' + this.pagination.current);
+            console.log('totalItens: ' + this.pagination.totalItens);
+            console.log('totalPages: ' + this.pagination.totalPages);
+            console.log('listNumbers: ' + this.pagination.listNumbers);
+            console.log('listPagination: ' + this.pagination.listPagination);
+            this.pagination.current = current +1;
+            this.books = this.pagination.listPagination[current];
         }
     },
     ready:function() {
         var self = this;
         self.$http.get('dataServer.json').then(function(response) {
-            //console.log(JSON.parse(response.data));
             self.books = JSON.parse(response.data);
+            //console.log(JSON.parse(response.data));
+            self.pagination.totalItens = self.books.length;
+            self.pagination.totalPages = Math.ceil(self.pagination.totalItens / self.pagination.maxPage);
+
+            var aux =[];
+            for(var k in self.books) {
+                aux.push(self.books[k]);
+                if(aux.length === self.pagination.maxPage) {
+                    self.pagination.listPagination.push(aux);
+                    aux = [];
+                }
+            }
+            if(aux.length > 0) {
+                self.pagination.listPagination.push(aux);
+            }
+
+            //console.log(self.pagination.listPagination);
+            self.books = self.pagination.listPagination[0];
         });
     }
 
